1065d54ecdc47b8f9ad10974d1f09e8b8042f269
Ahmad Jarara <floor13@jarmac.org>
2016-07-23 10:17:40 -0400

    multiterm incorporated, evil-leader, moved to bind

--- init.el
+++ init.el
@@ -1,3 +1,4 @@
+;; -------------------- PACKAGE INIT --------------------
 (require 'package)
 (package-initialize)
 
@@ -6,10 +7,6 @@
 (add-to-list 'package-archives '("melpa-stable" . "http://stable.melpa.org/packages/"))
 
 (setq package-enable-at-startup nil)
-
-;; just so I have the flexibility of changing install directory later on
-(setq default-directory "~/.emacs.d/")
-(setq my-config-dir (concat default-directory "config/"))
 
 ;; use-package automatic install
 (unless (package-installed-p 'use-package)
@@ -21,26 +18,23 @@
 
 (eval-when-compile
   (require 'use-package))
+;; a utility that removes a minor mode from the modeline
 (require 'diminish)
 (require 'bind-key)
-
 
 ;; Make sure to have downloaded archive description.
 (or (file-exists-p package-user-dir)
     (package-refresh-contents))
 
 ;; yes I know I should use org mode for this..
-;; TODO move all my stuff into a big init file
 ;; TODO not emacs related, make keyboard config a single shell script
 ;; TODO emacs related, find out the most used org capture template keywords
 ;; TODO xml parser! there's a lib released on reddit that might help with this, it's new! GET HYPE
 ;; . as super modifier? Only problem is ". " How about /, but that's too much. Q is also considered. need ideas. semicolon works GREAT
 ;; maybe it's a good idea to split shift usage. there was a guy who wrote something that ignored all input unless you used the right keys.
 ;; here's what I wanted to write initially: make the cursor color change based on modes. Yes I can look down but that's too slow. maybe some red or something, use the monokai theme as a guide. the defun color?
-
 ;; -------------------- NAKED-CONFIG --------------------
 ;; configuration that doesn't rely on any external packages, with the exception of bind-key, because it's very useful.
-
 
 ;; this is needed, otherwise emacsclient hangs upon exit when it has something in the kill ring
 (setq x-select-enable-clipboard-manager nil)
@@ -52,7 +46,6 @@
 
 ;; add in a useful visual goodie
 (visual-line-mode 1)
-
 
 ;; why is this the only way to set word wrap... -_-
 (toggle-word-wrap)
@@ -93,22 +86,6 @@
 (setq use-dialog-box nil)
 
 
-;; rebinding the weird stuff. the asterisk designates: don't override this, ever
-;; M-i shadows tab-to-tab-stop, it inserts tabs and I'm firmly a spaces guy. If I want to indent something, I'll use C-M-\, or do it by hand.
-;; imenu is alright, tbh. idk why I bound it here.
-(bind-key* "M-i" `imenu)
-
-;; needed to get around the whole C-i == tab debacle. don't have a Hyper key, or plan on configuring one, so this works decent enough.
-;; does not work, unfortunately. <tab> still yields init-file. need to replace it with something else.
-;;(define-key input-decode-map (kbd "C-i") (kbd "H-i"))
-;;(bind-key* "C-i" `my/find-init-file)
-
-
-;; no reason to have C-i bound to Tab when I have Tab right there.
-;; warning, this does not work if you run in a terminal. C-i and C-m are firmly linked to tab and return.
-
-;; top level keys I don't mind rebinding, in order of uselessness
-;; C-m C-q C-j M-u 
 
 
 ;; -------------------- PACKAGES ---------------------
@@ -119,10 +96,11 @@
 ;; TODO list packages by order of use 
 
 (use-package helm
+  :init
+  ;; helm sets this stuff off, and they're not gonna fix it: https://github.com/emacs-helm/helm/issues/1498#issue-154021209
+  (setq ad-redefinition-action 'accept) 
   :ensure t
-  :config
-  ;; literally the only thing I use helm and not ivy for. When ivy gets inline bindings, maybe I'll get rid of this for good. 
-  (global-set-key (kbd "M-x") `helm-M-x))
+  :bind* (("M-x" . helm-M-x)))
 
 (use-package ivy
   :ensure t
@@ -139,10 +117,9 @@
   (yas-global-mode))
 
 (use-package swiper
-   :ensure t
-   :config
-   (global-set-key (kbd "C-s") `swiper))
-
+  :ensure t
+  :bind* (("C-s" . swiper))
+  )
 
 (use-package ace-window
   :ensure t
@@ -157,21 +134,55 @@
   (slime-setup)
   (setq inferior-lisp-program "/usr/bin/sbcl"))
 
+(use-package circe
+  :ensure t
+  :config
+  (setq circe-reduce-lurker-spam t)
+  (setq circe-network-options
+	'(("ZNC"
+	   :tls t
+	   :host "jarmac.org"
+	   :port 6697
+	   :nick "alphor"
+	   ))))
+
 (use-package smooth-scrolling
   :ensure t
   :config
   (smooth-scrolling-mode 1)
   (setq smooth-scroll-margin 5))
 
-
 (use-package evil
-  ;; need to disable until I actually read the damn manual, then I will reenable
   ;; evil-leader is run before evil, so that leader keys work in scratch and messages 
   :init
-  ;; don't actually use this at all, just couldn't set it to nothing
   (setq evil-toggle-key "C-`")
+  ;; since I have evil everywhere now, this might be neat. only works in normal mode, obviously.
+
+  (use-package evil-leader
+    :config
+    (setq evil-leader/leader "SPC")
+    (evil-leader/set-key "v" `visual-line-mode)
+    (evil-leader/set-key "w" `toggle-word-wrap)
+    (evil-leader/set-key "g" `magit-status)
+    (global-evil-leader-mode)
+    )
+  
+  ;; don't actually use this at all, just couldn't set it to nothing
   :ensure t
   
+  ;; notice the lack of bind*. since I'm binding to a map, I don't want this to be global.
+  :bind* (:map evil-emacs-state-map
+	       ("C-y" . yank)
+	       ("C-r" . evil-paste-from-register)
+	       :map evil-normal-state-map
+	       ("j" . evil-next-visual-line)
+	       ("k" . evil-previous-visual-line))
+
+  :bind-keymap*
+  (("C-w" . evil-window-map))
+  
+  ;; the bind keyword lazy loads evil until you use one of the binds. I don't wanna do that, instead, I want it to load immediately.
+  :demand
   :config
   (evil-mode t)
   ;; the below is used to have emacs be the default state, but allow me to drop in to evil if need be.
@@ -179,38 +190,28 @@
   (progn
     (defalias 'evil-insert-state 'evil-emacs-state) ; http://stackoverflow.com/a/27794225/2932728
     (setq evil-default-state 'emacs)
-  ;; https://bitbucket.org/bastibe/.emacs.d/src/12d08ec90a6445787b028fa8640844a67182e96d/init.el?at=master&fileviewer=file-view-default
+    ;; https://bitbucket.org/bastibe/.emacs.d/src/12d08ec90a6445787b028fa8640844a67182e96d/init.el?at=master&fileviewer=file-view-default
     (define-key evil-emacs-state-map [escape] 'evil-normal-state)
-    (define-key evil-normal-state-map (kbd "j") 'evil-next-visual-line)
-    (define-key evil-normal-state-map (kbd "k") 'evil-previous-visual-line)
     )
+  ;; I didn't put the above define-key into the bind just because it makes more sense here. If I encounter a remapping of esc, I'd probably move it into bind*
   
-  ;; IDK about motion state, it blocks useful keys.
-  ;; that said, this isn't perfect. there are some things I'd like to modify.
+  ;; IDK about motion state, it blocks useful keys, like ? or h.
 
-  ;; I'd like C-y to 'yank' even while in normal mode.
-  (define-key evil-normal-state-map (kbd "C-y") 'yank)
-  
-  ;; todo alias 0 to C-a
-  ;; todo change cursor color based on emacs/normal state
+  ;; a quick way to differentiate which state I'm in without looking at the mode line, may change this later.
+  (setq evil-emacs-state-cursor `bar)
 
-  ;; I like the use of registers (while in insert mode), and I don't really need isearch backwards when I've got swiper which is a whole lot better
-  ;; notice I modify evil-emacs state, as I've aliased it to insert state.
-  (define-key evil-emacs-state-map (kbd "C-r") 'evil-paste-from-register)
-  
-  ;; marks are nice. would be really nice if there was a way to do global marks.
-  (define-key evil-normal-state-map (kbd "'") `evil-goto-mark)
-  
-  ;; since I have evil everywhere now, this might be neat. only works in normal mode, obviously.
-  (use-package evil-leader
-    :config
-    (evil-leader/set-leader ",")
-    )
   
   (use-package evil-visual-mark-mode
     :ensure t
     :config
     (evil-visual-mark-mode))
+  )
+
+;; has to be after evil has been loaded in. For some reason, evil does not want to let go of the C-z key.
+;; better than ansi-term. has sane defaults, allows me to M-x, C-h still operates, multi-term-next spawns a new shell if there aren't any, and cycles!
+(use-package multi-term
+  :ensure t
+  :bind* (("C-z" . multi-term-next))
   )
 
 (use-package elpy
@@ -233,9 +234,9 @@
   (global-whitespace-cleanup-mode))
 
 
-;;
 (use-package try
-    :ensure t)
+  :ensure t
+  )
 
 
 ;; THEME
@@ -251,9 +252,7 @@
   (setq org-directory "~/Documents/org/")
 
   (setq org-default-notes-file (concat org-directory "/notes.org"))
-  (define-key global-map (kbd "<f5>") `org-capture)
-
-
+  
   ;; capture templates that work, as of now.
   ;; for more info, check out http://orgmode.org/manual/Capture-templates.html
   (setq org-capture-templates
@@ -265,18 +264,71 @@
 	   "* %?\nEntered on %U\n  %i\n  %a")
           ("k" "KOL" entry (file+datetree "~/Documents/org/kol.org")
 	   "* %?\nEntered on %U\n %a")
+          ("a" "ascension" entry (file+datetree "~/Documents/org/kol-ascension.org")
+	   "* %?\nEntered on %U\n %a")
 	  ("m" "track" entry (file+datetree "~/Documents/org/track.org")
 	   "* %?\nEntered on %U\n")
 	  ("g" "grievances" entry (file+datetree "~/Documents/org/grievances.org")
 	   "* %?\nEntered on %U\n")
 	  )
 	)
+  :bind*
+  (("<f5>" . org-capture))
   )
 
+;; -------------------- TOP LEVEL BINDS --------------------
+;; uh... NOT TIED TO A SPECIFIC PACKAGE
 
-;; should I move this func to the my namespace?
-;; meh.
-(defun smarter-move-beginning-of-line (arg)
+;; M-x describe-personal-keybinds
+;; interestingly enough, this doesn't apply if you override-global-map. is that all the asterisk does? (override-global-map, not the small effect on desc-personal)
+;; list of top level binds I don't use, ever. (aka mostly highlight keys that I'd like to be encapsulated in visual mode)
+;; C-q M-q M-e M-y M-i M-o C-i (tab)
+;; M-c (upcase word? nty)
+;; M-u? maybe. M-k
+;; shadows means stock emacs ships with this bound, but I rebind it.
+
+;; shadows fill-paragraph
+(bind-key* "M-q" `ace-window)
+;; I don't know if this is the best idea, seeing as how I ALWAYS use ace-window, but it makes a little sense to do this with the evil windowing system.
+;; despite quoted-insert growing on me, maybe that's better reserved for something to be used in evil-leader, <leader> q or something, as that's definitely something I'll use in normal mode often.
+;; shadows quoted-insert
+(bind-key* "C-q" `ace-window)
+
+;; shadows universal arg, I think? Damn, I need to read the manual.
+(bind-key* "C-0" `text-scale-adjust)
+
+;; shadows move-to-window-line-top-bottom 
+(bind-key* "M-r" `delete-other-windows)
+
+;; would like this instead to just kill the buffer, or like rotate. I think I need some buffer management tool
+;; shadows kill-ring-save
+(bind-key* "M-w" `delete-window)
+
+;; I don't actually know what the name of the function is, but I know I don't need it. It's some typeface stuff.
+;; also, the function name here is misleading, it evaluates the whole top-level expression, from anywhere in the expression.
+;; shadows Set face:
+(bind-key* "M-o" `eval-defun)
+
+
+;; shadows tab-to-tab-stop
+(bind-key* "M-i" `my/find-init-file)
+;; shadows nothing that I know of.
+(bind-key "M-p" `my/find-projects)
+
+;; I type it just enough for it to be useful.
+;; maybe a cool package idea would be to suggest a more interactive way to define keybinds
+;; based on frequency of M-x invocations
+;; another would be a visualization of all keybinds of all minor/major modes installed, so you
+;; can find a key that you'll be fine with 
+;; shadows nothing that I know of, hence the lack of *
+(bind-key "C-h SPC" `which-key-show-top-level)
+
+
+
+;; -------------------- CUSTOM-FUNCTIONS ---------------------
+
+;; something useful from the emacs wiki? No way.
+(defun my/smarter-move-beginning-of-line (arg)
   "Move point back to indentation of beginning of line.
 
 Move point to the first non-whitespace character on this line.
@@ -301,23 +353,15 @@ point reaches the beginning or end of the buffer, stop there."
 
 ;; remap C-a to `smarter-move-beginning-of-line'
 (global-set-key [remap move-beginning-of-line]
-                'smarter-move-beginning-of-line)
+                'my/smarter-move-beginning-of-line)
 
-;; I type it enough for it to be useful.
-;; maybe a cool package idea would be to suggest a more interactive way to define keybinds
-;; based on frequency of M-x invocations
-;; another would be a visualization of all keybinds of all minor/major modes installed, so you
-;; can find a key that you'll be fine with 
-(global-set-key (kbd "C-h SPC") `which-key-show-top-level)
 
-(defun my/znc ()
-  (interactive)
-  (erc-tls
-   :server "jarmac.org"
-   :port 6697
-   ))
+
+;; -------------------- MY-MODE (to be pruned) ---------------------
 
 ;; planning on moving all this functionality to bind-key.
+;; got rid of this because I'd like to use top level binds for most of these extremely common operations. unfortunately, there is no easy way to unbind these temporarily without commenting the relevant lines out, then reloading the entirety of emacs. if there was a package that rebound stock keys or dynamically rebound them to something, that would be neat!
+;; oh jeeze, emacs thought of it already: describe-personal-keybindings
 
 (defvar my-prefix "C-j ")
 (defun my/prefix-add-to-map (map key-as-string function-symbol)
@@ -326,18 +370,8 @@ point reaches the beginning or end of the buffer, stop there."
 (defvar my/mode-map
   (let ((my-map (make-keymap)))
     
-    ;; top level binds
-    (define-key my-map (kbd "C-q") `ace-window)
-    ;;(define-key my-map (kbd "C-z") `eshell)
-;;    (define-key my-map (kbd "M-h") `switch-to-buffer)
-    (define-key my-map (kbd "M-r") `delete-other-windows)
-    (define-key my-map (kbd "M-w") `delete-window)
-    (define-key my-map (kbd "M-q") `kill-buffer)
     ;; this doesn't work well, it gets rebound to tab. need another keybind. C-m doesn't work either, for the same stupid reason.
     ;;(define-key my-map (kbd "C-i") `my/kill-other-window)
-    
-    ;; muddling in other keymaps
-    (define-key my-map (kbd "C-h SPC") `which-key-show-top-level)
     
     ;; under my/prefix
     (my/prefix-add-to-map my-map "V" `visual-line-mode)
@@ -346,9 +380,7 @@ point reaches the beginning or end of the buffer, stop there."
 
     (my/prefix-add-to-map my-map "C-g" `keyboard-quit) 
     (my/prefix-add-to-map my-map "C-j" `ace-window)
-    (my/prefix-add-to-map my-map "C-0" `text-scale-adjust)
     (my/prefix-add-to-map my-map "C-r" `org-capture)
-    (my/prefix-add-to-map my-map "C-q" `quoted-insert)
     (my/prefix-add-to-map my-map "C-f" `ffap)
     (my/prefix-add-to-map my-map "C-s" `magit-status)
     
@@ -356,6 +388,7 @@ point reaches the beginning or end of the buffer, stop there."
     (my/prefix-add-to-map my-map "C-t" `my/toggle-window-split)
     (my/prefix-add-to-map my-map "C-c" `my/find-emacs-config-file)
     (my/prefix-add-to-map my-map "C-p" `my/find-projects)
+    ;; return my-map
     my-map
     ))
 
@@ -368,13 +401,15 @@ point reaches the beginning or end of the buffer, stop there."
 ;; evaluate it. considering moving to johnw's bind-key so that I can declare these keybinds in use-package configs
 (my/mode)
 
+;; -------------------- FUNCTIONS --------------------
 	
 (defun my/kill-other-window ()
   (interactive)
   (if (= (count-windows) 2)
       (progn
 	(other-window 1)
-	(kill-buffer))
+	(kill-buffer)
+	(other-window 1))
     (error "This only works when there are two buffers!")))
 
 		  
