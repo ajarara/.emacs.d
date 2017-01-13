902a8643c5946a9ffe350e34c73f2f98de16c222
Ahmad Jarara <ajarara@jarmac.org>
2017-01-13 18:23:16 -0500

    State your goals! Love them. Be them.

--- init.el
+++ init.el
@@ -121,6 +121,8 @@
 
 (quelpa 'general)
 (use-package general
+  ;; maybe in the future make this config evil agnostic?
+  :if (featurep 'evil)
   :config
 
   ;; leader key binds
@@ -151,10 +153,7 @@
 
    "i" 'imenu
 
-  
    ) ;; closes general-define-key block
-
-
   
 ) ;; closes use-package general block
 
@@ -676,17 +675,3 @@ point reaches the beginning or end of the buffer, stop there."
 (toggle-debug-on-error)
 
 (message "Emacs config successfully loaded!")
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages
-   (quote
-    (which-key use-package try swiper slime quelpa projectile pdf-tools nix-mode monokai-theme magit ledger-mode jade htmlize helm general expand-region evil-visual-mark-mode ess elpy circe-actions circe buttercup ace-window))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
