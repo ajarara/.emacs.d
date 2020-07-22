394ef192a5ade1bdbc3f93202d35fae0d33bcee1
Ahmad Jarara <ajarara94@gmail.com>
2020-07-22 08:52:23 -0400

    Add first lib

---
+++ lib/setup-visuals.el
@@ -0,0 +1,42 @@
+;; can be recovered with <F10>
+(tool-bar-mode -1)
+
+;; not needed, just look at the mode line for gauging where you are in the buffer
+(scroll-bar-mode -1)
+
+;; wait which one is which
+(menu-bar-mode -1)
+
+;; you can set this to some arbitrary number so that it will blink for that number of times. I don't like blinking.
+(blink-cursor-mode 0)
+
+;; pretty quotes can't be jumped to easily.
+(setq text-quoting-style 'grave)
+
+;; Might be considered a hindrance, but I rarely need more than 80
+;; columns while programming and would rather not have to deal with
+;; horizontal scrolling. These two settings are neat for prose, and
+;; org mode.
+(visual-line-mode 1)
+
+;; I don't know how I feel using hl-line-mode on EVERY
+;; buffer. Instead, let's just add the ones that make sense.
+(defvar my-hl-line-mode-hook-list
+  `(prog-mode-hook
+    circe-mode-hook))
+
+(dolist (this-mode-hook my-hl-line-mode-hook-list)
+  (add-hook this-mode-hook `hl-line-mode))
+
+
+(defun setup-disable-visuals-unload-function ()
+  (tool-bar-mode 1)
+  (scroll-bar-mode 1)
+  (menu-bar-mode 1)
+  (blink-cursor-mode 10)
+  (setq text-quoting-style nil)
+  (visual-line-mode -1)
+  (dolist (this-mode-hook my-hl-line-mode-hook-list)
+    (remove-hook this-mode-hook `hl-line-mode)))
+
+(provide 'setup-visuals)
