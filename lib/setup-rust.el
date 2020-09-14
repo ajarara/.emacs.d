8c49176ba6b5850f6de0d65500caad0264ec2410
Ahmad Jarara <git@ajarara.io>
2020-09-13 21:02:34 -0400

    Hook in lsp on rust-mode

--- lib/setup-rust.el
+++ lib/setup-rust.el
@@ -1,13 +1,10 @@
-;; Oh, you knew it was coming.
-(straight-use-package 'rust-mode)
-  (use-package rust-mode)
-(straight-use-package 'cargo)
-  (use-package cargo)
-  (add-hook 'rust-mode-hook 'cargo-minor-mode)
+(use-package rust-mode
+  :mode "\\.rs\\"
+  :hook lsp)
 
-  ;; somewhere I read that using add-to-list is not enough, even though it mutates the list..
-  ;; I'll leave out the setq at first and see if emacs cooperates 
-(add-to-list 'auto-mode-alist '("\\.rs" . rust-mode))
+(use-package cargo
+  :hook rust-mode)
+
 
 (provide 'setup-rust)
 
