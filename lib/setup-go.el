4c61a04ec998d780f64d0e3fb6d7cdc66dc42824
Ahmad Jarara <git@ajarara.io>
2020-10-02 17:39:12 -0400

    Provide setup-go, add direnv-mode

--- lib/setup-go.el
+++ lib/setup-go.el
@@ -2,4 +2,7 @@
 (use-package go-mode
   :config
   (add-hook 'go-mode-hook 'lsp)
+  (add-hook 'go-mode-hook 'direnv-mode)
   (add-to-list 'auto-mode-alist'("\\.go" . go-mode)))
+
+(provide 'setup-go)
