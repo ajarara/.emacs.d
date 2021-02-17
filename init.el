c01c6841b668d5eb833ea0a44b2c84589b7f46f3
Ahmad Jarara <git@ajarara.io>
2021-02-17 11:38:50 -0500

    Use bar always

--- init.el
+++ init.el
@@ -187,11 +187,8 @@
 
 ;; You split along the horizontal axis, I guess. Sure.
 (setq split-height-threshold nil)
-
-;; This works on a small 11 in screen, but I have a big screen with a docking station at home.
-;; It would be great to preserve this behavior by relative size of screen, not absolute.
 (setq split-width-threshold 140)
-
+(setq-default cursor-type 'bar)
 
 (setq-default indent-tabs-mode nil)
 
