ecace396486c17802328e16961206c732630f6ef
Ahmad Jarara <ajarara94@gmail.com>
2022-01-09 14:00:20 -0500

    Mark right command as control

--- init.el
+++ init.el
@@ -441,6 +441,8 @@ point reaches the beginning or end of the buffer, stop there."
   :init
   (setq deft-extension "gpg"))
 
+(setq ns-right-command-modifier 'control)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
