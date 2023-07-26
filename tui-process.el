033b78a9f3423df6fe8b5aaa1f0ee9b2505512d1
Ahmad Jarara <git@ajarara.io>
2023-07-26 09:35:11 -0400

    tui-process works

--- tui-process.el
+++ tui-process.el
@@ -44,69 +44,61 @@
   (let ((prev-state (plist-get (tui-get-state component) state-key)))
     (if (or (not prev-state)
             (not (eq dependencies (tui-process-state-dependencies prev-state))))
-      (let ((command (funcall command-creator))
-            (stdout-filter (lambda (proc text)
-                             (tui-set-state
-                              component
-                              (tui-process--plist-overwrite
-                               state-key
-                               ;; it is not safe to reference prev-state here                               
-                               (tui-get-state component)
-                               (lambda (proc-state)
-                                 ;; update the stdout deltas, keep everything else
-                                 (tui-process-state--create
-                                  :process (tui-process-state-process proc-state)
-                                  :stdout-deltas (cons text (tui-process-state-stdout-deltas proc-state))
-                                  :stderr-deltas (tui-process-state-stderr-deltas proc-state)
-                                  :dependencies (tui-process-state-dependencies proc-state)))))))
-            (stderr-filter (lambda (proc text)
-                             (let ((new-state
-                                    (tui-process--plist-overwrite
-                                     state-key
-                                     (tui-get-state component)
-                                     (lambda (proc-state)
-                                       ;; update the stderr deltas, keep everything else
-                                       (tui-process-state--create
-                                        :process (tui-process-state-process proc-state)
-                                        :stdout-deltas (tui-process-state-stdout-deltas proc-state)
-                                        :stderr-deltas (cons text (tui-process-state-stderr-deltas proc-state))
-                                        :dependencies (tui-process-state-dependencies proc-state))))))
-                             (tui-set-state
-                              component
-                              (tui-process--plist-overwrite
-                               state-key
-                               ;; it is not safe to reference prev-state here
-                               (tui-get-state component)
-                               (lambda (proc-state)
-                                 ;; update the stderr deltas, keep everything else
-                                 (tui-process-state--create
-                                  :process (tui-process-state-process proc-state)
-                                  :stdout-deltas (tui-process-state-stdout-deltas proc-state)
-                                  :stderr-deltas (cons text (tui-process-state-stderr-deltas proc-state))
-                                  :dependencies (tui-process-state-dependencies proc-state))))))))
+      (let* ((command (funcall command-creator))
+             (stdout-filter (lambda (proc text)
+                              (tui-set-state
+                               component
+                               (tui-process--plist-overwrite
+                                state-key
+                                ;; it is not safe to reference prev-state here                               
+                                (tui-get-state component)
+                                (lambda (proc-state)
+                                  ;; update the stdout deltas, keep everything else
+                                  (tui-process-state--create
+                                   :process (tui-process-state-process proc-state)
+                                   :stdout-deltas (cons text (tui-process-state-stdout-deltas proc-state))
+                                   :stderr-deltas (tui-process-state-stderr-deltas proc-state)
+                                   :dependencies (tui-process-state-dependencies proc-state)))))))
+             (stderr-filter (lambda (proc text)
+                              (tui-set-state
+                               component
+                               (tui-process--plist-overwrite
+                                state-key
+                                ;; it is not safe to reference prev-state here
+                                (tui-get-state component)
+                                (lambda (proc-state)
+                                  ;; update the stderr deltas, keep everything else
+                                  (tui-process-state--create
+                                   :process (tui-process-state-process proc-state)
+                                   :stdout-deltas (tui-process-state-stdout-deltas proc-state)
+                                   :stderr-deltas (cons text (tui-process-state-stderr-deltas proc-state))
+                                   :dependencies (tui-process-state-dependencies proc-state)))))))
+             (new-proc-state
+              (tui-process-state--create
+               :process (tui-process--execute command stdout-filter stderr-filter)
+               :stdout-deltas '()
+               :stderr-deltas '()
+               :dependencies dependencies)))
         (tui-set-state
          component
          (tui-process--plist-overwrite
           state-key
           (tui-get-state component)
-          (lambda (prev-state)  ;; here prev-state is always the same
+          (lambda (prev-state)
             ;; tear down the old one as we're replacing it with a new one
             (when prev-state
               (kill-process (tui-process-state-process prev-state)))
-            (tui-process-state--create
-             :process (tui-process--execute command stdout-filter stderr-filter)
-             :stdout-deltas '()
-             :stderr-deltas '()
-             :dependencies dependencies)))))
+            new-proc-state)))
+        new-proc-state)
       prev-state)))
 
-(tui-defun-2 my-component (&this this &state example)
+(tui-defun-2 my-component (&this this)
   "documentation is good"
-  (let ((proc (tui-process-create this :example (lambda () '("logger" "-s" "'please clap'")) nil)))
-    ;; (tui-div
-    ;;  (format "stdout: %s" (string-join (reverse (tui-process-state-stdout-deltas proc))))
-    ;;  (format "stderr: %s" (string-join (reverse (tui-process-state-stderr-deltas proc)))))))
-    (string-join (reverse (tui-process-state-stderr-deltas proc)))))
+  (if-let ((proc (tui-process-create this :example (lambda () '("logger" "-s" "'please clap'")) nil)))
+    (tui-div
+     (format "stdout: %s" (string-join (reverse (tui-process-state-stdout-deltas proc))))
+     (format "stderr: %s" (string-join (reverse (tui-process-state-stderr-deltas proc)))))))
+
 
 (let* ((buffer (get-buffer-create "*my-component*"))
        (component (my-component)))
