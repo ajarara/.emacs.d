b2723800722220a168cf46427a44971e4c77def2
Ahmad Jarara <git@ajarara.io>
2023-09-03 20:13:32 -0400

    async compilation shouldn't pop the buffer

--- custom.el
+++ custom.el
@@ -3,6 +3,7 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(native-comp-async-report-warnings-errors 'silent)
  '(safe-local-variable-values
    '((eval let
            ((root-dir-unexpanded
