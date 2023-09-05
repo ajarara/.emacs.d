8d9e329a476662572a1097e388cdef4d17ffa8f5
Ahmad Jarara <git@ajarara.io>
2023-09-04 21:53:12 -0400

    Cleanup

--- lisp/tui-process.el
+++ lisp/tui-process.el
@@ -17,8 +17,6 @@
         (done-states '(exit signal)))
     (memql proc-status done-states)))
 
-
-;; to test stderr use 'logger -s "this-will-be-sent-to-stderr-filter"'
 (cl-defun tui-process--execute (&key command stdout-filter stderr-filter sentinel)
   (let* ((stderr-buf (get-buffer-create (symbol-name (gensym " anonymous-stderr-buffer-"))))
          (process (make-process
@@ -30,7 +28,6 @@
          (stderr-process (get-buffer-process stderr-buf)))
     (set-process-filter stderr-process stderr-filter)
     process))
-
 
 (defun tui-use-process (component command)
   (let* ((proc-status-state (tui-use-state component nil))
@@ -78,67 +75,5 @@
      :process-status (car proc-status-state)
      :stdout-deltas (car stdout-state)
      :stderr-deltas (car stderr-state))))
-
-;; (tui-defun-2 tui-process-test-process (&this this)
-;;   "tui-process-test-process"
-;;   (let* ((proc-state (tui-use-process this '("logger" "-s" "'this will be sent to stderr'")))
-;;          ;;(_ (message "%s" (tui-process-state-stderr-deltas proc-state)))
-;;          (stderr (string-join (reverse (tui-process-state-stderr-deltas proc-state)) " ")))
-;;     stderr))
-         
-;; (tui-defun-2 tui-process-test-component-states (&this this)
-;;   "tui-process-test-component"
-;;   (let ((state (tui-use-state this 15)))
-;;     (cond
-;;      ((eql 15 (car state))
-;;       (message "first render!")
-;;       (funcall (cadr state) 16))
-;;      ((eql 16 (car state))
-;;       (message "second render!")
-;;       (funcall (cadr state) 17)))
-;;     (format "curr-state: %s trailing" (car state))))
-
-;; (tui-defun-2 tui-process-test-component-effects (&this this)
-;;   "tui-process-test-component"
-;;   (let* ((state (tui-use-state this 15))
-;;          (_ (tui-use-effect this
-;;                             (car state)
-;;                             (lambda ()
-;;                               (cond
-;;                                ((eql 15 (car state))
-;;                                 (message "first render!")
-;;                                 (funcall (cadr state) 16)
-;;                                 (message "first render after set-state")
-;;                                 ;; why is this being run _after_ second render?
-;;                                 (lambda () (message "teardown of first observing %s" (car state))))
-;;                                ((eql 16 (car state))
-;;                                 (message "second render!")
-;;                                 (funcall (cadr state) 17)
-;;                                 (lambda () (message "teardown of second observing %s" (car state))))
-;;                                ((eql 17 (car state))
-;;                                 (message "third render!")
-;;                                 (lambda () (message "teardown of component!"))))) )))
-;;     (format "curr-state: %s trailing" (car state))))
-
-;; (tui-defun-2 tui-process-test-component (&this this)
-;;   "tui-process-test-component"
-;;   (let* ((state (tui-use-state this 15))
-;;          (state-updater (cadr state))
-;;          (cb (tui-use-callback
-;;               this
-;;               state-updater
-;;               (lambda ()
-;;                 (
-                               
-
-;; (defun tui-process-test ()
-;;   (interactive)
-;;   (let* ((buffer (get-buffer-create "*tui-process-test*"))
-;;          (component (tui-process-test-process)))
-;;     (tui-render-element
-;;      (tui-buffer
-;;       :buffer buffer
-;;       component))
-;;     (switch-to-buffer buffer)))
 
 (provide 'tui-process)
--- lisp/tui-test.el
+++ lisp/tui-test.el
@@ -4,39 +4,6 @@
 (require 'tui-hooks)
 (require 'tui-use-process-buffer)
 
-;; (tui-defun-2 tui-process-test-component-states (&this this)
-;;   "tui-process-test-component"
-;;   (let ((state (tui-use-state this 15)))
-;;     (cond
-;;      ((eql 15 (car state))
-;;       (message "first render!")
-;;       (funcall (cadr state) 16))
-;;      ((eql 16 (car state))
-;;       (message "second render!")
-;;       (funcall (cadr state) 17)))
-;;     (format "curr-state: %s trailing" (car state))))
-
-;; (tui-defun-2 tui-process-test-component-effects (&this this)
-;;   "tui-process-test-component"
-;;   (let* ((state (tui-use-state this 15))
-;;          (_ (tui-use-effect this
-;;                             (car state)
-;;                             (lambda ()
-;;                               (cond
-;;                                ((eql 15 (car state))
-;;                                 (message "first render!")
-;;                                 (funcall (cadr state) 16)
-;;                                 (message "first render after set-state")
-;;                                 ;; why is this being run _after_ second render?
-;;                                 (lambda () (message "teardown of first observing %s" (car state))))
-;;                                ((eql 16 (car state))
-;;                                 (message "second render!")
-;;                                 (funcall (cadr state) 17)
-;;                                 (lambda () (message "teardown of second observing %s" (car state))))
-;;                                ((eql 17 (car state))
-;;                                 (message "third render!")
-;;                                 (lambda () (message "teardown of component!"))))) )))
-;;     (format "curr-state: %s trailing" (car state))))
 
 (tui-defun-2 tui-process-test-process (&this this)
   "tui-process-test-process"
@@ -46,30 +13,24 @@
         (with-current-buffer stderr-buffer
           (buffer-string)))))
 
-
 (tui-defun-2 tui-process-test-counter (&this this)
   "tui-process-test-counter"
   (let* ((elapsed-time-state (tui-use-state this 0))
          (elapsed-time-updater (cadr elapsed-time-state))
          (elapsed-time (car elapsed-time-state)))
-    (tui-use-effect
-     this
-     elapsed-time-updater
-     (lambda ()
-       (let ((timer
-              (run-at-time
-               0
-               1
-               (lambda ()
-                 (funcall elapsed-time-updater
-                          (lambda (curr-time) (1+ curr-time)))))))
-         (lambda ()
-           (cancel-timer timer)))))
+    (tui-use-inferred-effect this
+      (let ((timer
+             (run-at-time
+              0
+              1
+              (lambda ()
+                (funcall elapsed-time-updater #'1+)))))
+        (lambda ()
+          (cancel-timer timer))))
     (format "%s" elapsed-time)))
-
 
 (defun tui-process-test ()
   (interactive)
   (tui-render-with-buffer
       (get-buffer-create "*tui-process-test*")
-    (tui-process-test-process)))
+    (tui-process-test-counter)))
