9525671160a73646c04e2af47103a2942fda577b
Ahmad Jarara <git@ajarara.io>
2024-05-19 15:37:14 -0400

    Update customs

--- lisp/custom.el
+++ lisp/custom.el
@@ -5,7 +5,10 @@
  ;; If there is more than one, they won't work right.
  '(native-comp-async-report-warnings-errors 'silent)
  '(safe-local-variable-values
-   '((eval add-to-list 'completion-ignored-extensions ".go")
+   '((geiser-guile-binary "guix" "repl")
+     (geiser-insert-actual-lambda)
+     (geiser-repl-per-project-p . t)
+     (eval add-to-list 'completion-ignored-extensions ".go")
      (eval let
            ((root-dir-unexpanded
              (locate-dominating-file default-directory ".dir-locals.el")))
