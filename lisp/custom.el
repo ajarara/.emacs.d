b2e319edfa29af3ef04f898e8b16497b2b031ac3
Ahmad Jarara <ajarara@fastmail.com>
2025-08-14 15:40:59 -0400

    Bunch of updates to custom.el

--- lisp/custom.el
+++ lisp/custom.el
@@ -7,96 +7,99 @@
  '(magit-log-margin-show-committer-date t)
  '(native-comp-async-report-warnings-errors 'silent)
  '(safe-local-variable-values
-   '((etags-regen-ignores "test/manual/etags/")
+   '((eval and buffer-file-name
+           (not (eq major-mode 'package-recipe-mode))
+           (or (require 'package-recipe-mode nil t)
+               (let ((load-path (cons "../package-build" load-path)))
+                 (require 'package-recipe-mode nil t)))
+           (package-recipe-mode))
+     (etags-regen-ignores "test/manual/etags/")
      (etags-regen-regexp-alist
-      (("c" "objc")
-       "/[ \11]*DEFVAR_[A-Z_ \11(]+\"\\([^\"]+\\)\"/\\1/" "/[ \11]*DEFVAR_[A-Z_ \11(]+\"[^\"]+\",[ \11]\\([A-Za-z0-9_]+\\)/\\1/"))
+      (("c" "objc") "/[ \11]*DEFVAR_[A-Z_ \11(]+\"\\([^\"]+\\)\"/\\1/"
+       "/[ \11]*DEFVAR_[A-Z_ \11(]+\"[^\"]+\",[ \11]\\([A-Za-z0-9_]+\\)/\\1/"))
      (eval with-eval-after-load 'git-commit
            (add-to-list 'git-commit-trailers "Change-Id"))
      (visual-line-mode . t)
      (eval with-eval-after-load 'tempel
-           (if
-               (stringp tempel-path)
-               (setq tempel-path
-                     (list tempel-path)))
+           (if (stringp tempel-path)
+               (setq tempel-path (list tempel-path)))
            (let
                ((guix-tempel-snippets
                  (concat
                   (expand-file-name "etc/snippets/tempel"
-                                    (locate-dominating-file default-directory ".dir-locals.el"))
+                                    (locate-dominating-file
+                                     default-directory
+                                     ".dir-locals.el"))
                   "/*.eld")))
-             (unless
-                 (member guix-tempel-snippets tempel-path)
+             (unless (member guix-tempel-snippets tempel-path)
                (add-to-list 'tempel-path guix-tempel-snippets))))
-     (major-mode . shell-mode)
-     (geiser-guile-binary "guix" "repl")
-     (geiser-insert-actual-lambda)
-     (geiser-repl-per-project-p . t)
+     (major-mode . shell-mode) (geiser-guile-binary "guix" "repl")
+     (geiser-insert-actual-lambda) (geiser-repl-per-project-p . t)
      (eval add-to-list 'completion-ignored-extensions ".go")
      (eval let
            ((root-dir-unexpanded
-             (locate-dominating-file default-directory ".dir-locals.el")))
+             (locate-dominating-file default-directory
+                                     ".dir-locals.el")))
            (when root-dir-unexpanded
              (let*
                  ((root-dir
                    (file-local-name
                     (expand-file-name root-dir-unexpanded)))
-                  (root-dir*
-                   (directory-file-name root-dir)))
-               (unless
-                   (boundp 'geiser-guile-load-path)
+                  (root-dir* (directory-file-name root-dir)))
+               (unless (boundp 'geiser-guile-load-path)
                  (defvar geiser-guile-load-path 'nil))
                (make-local-variable 'geiser-guile-load-path)
                (require 'cl-lib)
-               (cl-pushnew root-dir* geiser-guile-load-path :test #'string-equal))))
+               (cl-pushnew root-dir* geiser-guile-load-path :test
+                           #'string-equal))))
      (diff-add-log-use-relative-names . t)
      (vc-git-annotate-switches . "-w")
-     (eval progn
-           (require 'lisp-mode)
-           (defun emacs27-lisp-fill-paragraph
-               (&optional justify)
+     (eval progn (require 'lisp-mode)
+           (defun emacs27-lisp-fill-paragraph (&optional justify)
              (interactive "P")
-             (or
-              (fill-comment-paragraph justify)
-              (let
-                  ((paragraph-start
-                    (concat paragraph-start "\\|\\s-*\\([(;\"]\\|\\s-:\\|`(\\|#'(\\)"))
-                   (paragraph-separate
-                    (concat paragraph-separate "\\|\\s-*\".*[,\\.]$"))
-                   (fill-column
-                    (if
-                        (and
-                         (integerp emacs-lisp-docstring-fill-column)
-                         (derived-mode-p 'emacs-lisp-mode))
-                        emacs-lisp-docstring-fill-column fill-column)))
-                (fill-paragraph justify))
-              t))
-           (setq-local fill-paragraph-function #'emacs27-lisp-fill-paragraph))
+             (or (fill-comment-paragraph justify)
+                 (let
+                     ((paragraph-start
+                       (concat paragraph-start
+                               "\\|\\s-*\\([(;\"]\\|\\s-:\\|`(\\|#'(\\)"))
+                      (paragraph-separate
+                       (concat paragraph-separate
+                               "\\|\\s-*\".*[,\\.]$"))
+                      (fill-column
+                       (if
+                           (and
+                            (integerp emacs-lisp-docstring-fill-column)
+                            (derived-mode-p 'emacs-lisp-mode))
+                           emacs-lisp-docstring-fill-column
+                         fill-column)))
+                   (fill-paragraph justify))
+                 t))
+           (setq-local fill-paragraph-function
+                       #'emacs27-lisp-fill-paragraph))
      (eval with-eval-after-load 'yasnippet
            (let
                ((guix-yasnippets
                  (expand-file-name "etc/snippets/yas"
-                                   (locate-dominating-file default-directory ".dir-locals.el"))))
-             (unless
-                 (member guix-yasnippets yas-snippet-dirs)
+                                   (locate-dominating-file
+                                    default-directory ".dir-locals.el"))))
+             (unless (member guix-yasnippets yas-snippet-dirs)
                (add-to-list 'yas-snippet-dirs guix-yasnippets)
                (yas-reload-all))))
      (geiser-default-implementation quote guix)
      (eval let
            ((root-dir-unexpanded
-             (locate-dominating-file default-directory ".dir-locals.el")))
+             (locate-dominating-file default-directory
+                                     ".dir-locals.el")))
            (when root-dir-unexpanded
              (let*
-                 ((root-dir
-                   (expand-file-name root-dir-unexpanded))
-                  (root-dir*
-                   (directory-file-name root-dir)))
-               (unless
-                   (boundp 'geiser-guile-load-path)
+                 ((root-dir (expand-file-name root-dir-unexpanded))
+                  (root-dir* (directory-file-name root-dir)))
+               (unless (boundp 'geiser-guile-load-path)
                  (defvar geiser-guile-load-path 'nil))
                (make-local-variable 'geiser-guile-load-path)
                (require 'cl-lib)
-               (cl-pushnew root-dir* geiser-guile-load-path :test #'string-equal))))
+               (cl-pushnew root-dir* geiser-guile-load-path :test
+                           #'string-equal))))
      (eval setq-local guix-directory
            (locate-dominating-file default-directory ".dir-locals.el"))
      (eval modify-syntax-entry 43 "'")
